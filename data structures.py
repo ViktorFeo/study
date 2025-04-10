f8af4bb68c27cd3f4a6cd4a13ec508c43be0ce1d
ViktorFeo <vsf.viktor@gmail.com>
2025-04-10 15:12:04 +0300

    python data structures

--- data structures.py
+++ data structures.py
@@ -2,6 +2,10 @@
 2. Структуры данных (3-4 дня)
 2.1 Списки, кортежи, множества, словари
 2.2 Методы работы с ними (sort, map, filter, lambda)
+# sort() – сортирует список на месте (изменяет исходный список).
+# sorted() – возвращает новый отсортированный список, не изменяя исходный.
+# map() – Применяет функцию ко всем элементам коллекции.
+# filter() - Оставляет только те элементы, которые удовлетворяют условию.
 2.3 List/Dict comprehensions
 2.4 Итераторы и генераторы
 
@@ -81,11 +85,13 @@ for key, value in d.items():
     d2.update({value: key})
 print(d2)
 # Дан список слов. Посчитайте частоту каждого слова.
-from collections import defaultdict
 words = ["xthtp", "ckjdfhb", "xthtp", "word", "ckjdfhb", "xthtp"]
-frequency = defaultdict(int)
+frequency = {}
 for word in words:
-    frequency[word] += 1
+    if word in frequency:
+        frequency[word] += 1
+    else:
+        frequency[word] = 1
 
 for word, count in frequency.items():
     print(f"Слово '{word}' встречается {count} раз(а)")
@@ -94,3 +100,38 @@ d1 = {'a': 1, 'b': 3}
 d2 = {'b': 2}
 merged = {**d1, **d2}
 print(merged)
+# Отсортируйте список строк по длине.
+words = ["apple", "banana", "cherry", "date"]
+words.sort(key=len)
+print(words)
+# Дан список кортежей (имя, возраст). Отсортируйте его по возрасту в порядке убывания.
+people = [("Alice", 25), ("Bob", 30), ("Charlie", 20)]
+people_sorted = sorted(people, key=lambda x: x[1], reverse=True)
+print(people_sorted)
+print(people[1][1])
+# Преобразуйте список чисел в список строк.
+numbers = [1, 2, 3, 4]
+str_numbers = list(map(str, numbers))
+print(str_numbers)
+# Дан список имён. Создайте список приветствий: "Hello, {name}!".
+names = ["Alice", "Bob", "Charlie"]
+greet = list(map(lambda x: f'Hello, {x}', names))
+print(greet)
+# Отфильтруйте список чисел, оставив только положительные.
+numbers = [-2, -1, 0, 1, 2]
+positive = list(filter(lambda x: x > 0, numbers))
+print(positive)
+# Дан список слов. Оставьте только те, которые начинаются на букву 'a'.
+words = ["apple", "banana", "avocado", "cherry"]
+new_words = list(filter(lambda x: x.startswith('a'), words))
+print(new_words)
+# Возведите все числа в квадрат и оставьте только чётные результаты.
+numbers = [1, 2, 3, 4, 5, 6]
+new_numbers = list(map(lambda x: x ** 2, numbers))
+new_numbers = list(filter(lambda x: x % 2 == 0, new_numbers))
+print(new_numbers)
+# Из списка строк выберите те, длина которых >3, и преобразуйте их в верхний регистр.
+words = ["cat", "dog", "elephant", "fox"]
+new_words = list(filter(lambda x: len(x) > 3, words))
+new_words = list(map(lambda x: x.upper(), new_words))
+print(new_words)
